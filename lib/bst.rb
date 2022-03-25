4d7e13f9d6825971cc14a55eb23efdd18012301d
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-25 09:47:00 -0500

    Working find and delete methods

--- lib/bst.rb
+++ lib/bst.rb
@@ -47,6 +47,39 @@ class Tree
     root
   end
 
+  def find_smallest(root)
+    root = root.left until root.left.nil?
+    root
+  end
+
+  def delete(value, root = @root)
+    return root if root.nil?
+
+    if value < root.data
+      root.left = delete(value, root.left)
+    elsif value > root.data
+      root.right = delete(value, root.right)
+    elsif root.left.nil?
+      root = root.right
+    elsif root.right.nil?
+      root = root.left
+    else
+      holder = find_smallest(root.right)
+      root.data = holder.data
+      root.right = delete(holder.data, root.right)
+    end
+    root
+  end
+
+  def find(value, root = @root)
+    return root if root.data == value
+    if value < root.data
+      find(value, root.left)
+    else
+      find(value, root.right)
+    end
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -54,6 +87,9 @@ class Tree
   end
 end
 
-bst = Tree.new(Array.new(15) { rand(1..100) })
+# bst = Tree.new(Array.new(15) { rand(1..100) })
+bst = Tree.new([20, 30, 40, 50, 60, 70, 80])
 
 bst.pretty_print
+
+p bst.find(40)
